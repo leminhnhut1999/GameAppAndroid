e99a8eb4b77012bf9a99122fce2524dfc6d57fbc
=lmnhat199x <56436312+lmnhat199x@users.noreply.github.com>
2020-01-04 03:45:18 +0700

    Nhat commit

--- app/src/main/java/com/example/myapplication/CauHoi.java
+++ app/src/main/java/com/example/myapplication/CauHoi.java
@@ -11,17 +11,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CauHoi extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>  {
 //    private Button btn_Caua, btn_Caub, btn_Cauc, btn_Caud;
@@ -41,7 +50,6 @@ public class CauHoi extends AppCompatActivity implements LoaderManager.LoaderCal
         buttons[3] = findViewById(R.id.btn_CauD);
         txt_Noidung = findViewById(R.id.txt_CauHoi);
         txt_thoigian = (TextView)findViewById(R.id.textView4);
-
         if(getSupportLoaderManager().getLoader(0)!=null)
         {
             getSupportLoaderManager().initLoader(0, null, this);
@@ -108,7 +116,98 @@ public class CauHoi extends AppCompatActivity implements LoaderManager.LoaderCal
 
     public void tro_giup_khan_gia(View view)
     {
+        int a,b,c,d;
         final Dialog dialog = new Dialog(this);
+        int tong = 100;
+        Random random =new Random();
+        String dapAn = cauHoiArrayList.get(0).getDapan();
+
+
+        dialog.setContentView(R.layout.barchart);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT);
+        Button btn = dialog.findViewById(R.id.btn_barchart);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        if(buttons[0].getText().toString().equals(dapAn)){
+            a = random.nextInt((tong-45)+1)+45;
+            tong -= a;
+            b = random.nextInt((tong-0)+1)+0;
+            tong -= b;
+            c = random.nextInt((tong-0)+1)+0;
+            tong -= c;
+            d = random.nextInt((tong-0)+1)+0;
+            openBarChart(dialog,a,b,c,d);
+        }
+        if(buttons[1].getText().toString().equals(dapAn)){
+            b = random.nextInt((tong-45)+1)+45;
+            tong -= b;
+            a = random.nextInt((tong-0)+1)+0;
+            tong -= a;
+            c = random.nextInt((tong-0)+1)+0;
+            tong -= c;
+            d = random.nextInt((tong-0)+1)+0;
+            openBarChart(dialog,a,b,c,d);
+        }
+        if(buttons[2].getText().toString().equals(dapAn)){
+            c = random.nextInt((tong-45)+1)+45;
+            tong -= c;
+            b = random.nextInt((tong-0)+1)+0;
+            tong -= b;
+            a = random.nextInt((tong-0)+1)+0;
+            tong -= a;
+            d = random.nextInt((tong-0)+1)+0;
+            openBarChart(dialog,a,b,c,d);
+        }
+        if(buttons[3].getText().toString().equals(dapAn)){
+            d = random.nextInt((tong-45)+1)+45;
+            tong -= d;
+            b = random.nextInt((tong-0)+1)+0;
+            tong -= b;
+            c = random.nextInt((tong-0)+1)+0;
+            tong -= c;
+            a = random.nextInt((tong-0)+1)+0;
+            openBarChart(dialog,a,b,c,d);
+        }
+
+        dialog.show();
+    }
+    public void openBarChart(Dialog dialog,int a, int b, int c, int d){
+        BarChart barChart =dialog.findViewById(R.id.chart);
+        ArrayList<BarEntry> datas = new ArrayList<>();
+        datas.add(new BarEntry(0,a));
+        datas.add(new BarEntry(1,b));
+        datas.add(new BarEntry(2,c));
+        datas.add(new BarEntry(3,d));
+        BarDataSet barDataSet =new BarDataSet(datas,"");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextSize(20f);
+        BarData barData = new BarData(barDataSet);
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        final String [] labels = new String[]{"A","B","C","D"};
+        IndexAxisValueFormatter formatter = new IndexAxisValueFormatter(labels);
+        xAxis.setTextSize(20f);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(formatter);
+        barChart.setData(barData);
+        //Khong ve luoi
+        xAxis.setDrawAxisLine(false);
+        barChart.getAxisLeft().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+
+        barChart.getLegend().setEnabled(false);
+        barChart.getDescription().setEnabled(false);
+
+        barChart.setDoubleTapToZoomEnabled(false);
+        barChart.setTouchEnabled(false);
+
+        barChart.animateXY(5000,5000);
+        barChart.invalidate();
 
     }
 
@@ -220,5 +319,24 @@ public class CauHoi extends AppCompatActivity implements LoaderManager.LoaderCal
             Toast.makeText(this,"Bạn đã trả lời sai",Toast.LENGTH_LONG).show();
             startActivity(intent);
         }
+    }
+
+    public void tro_giup_goi_dien_thoai(View view) {
+
+        String dapAn = cauHoiArrayList.get(0).getDapan();
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.MATCH_PARENT);
+        Button btn = dialog.findViewById(R.id.btn_thoat);
+        TextView txt = dialog.findViewById(R.id.txt_dapan);
+        txt.setText(dapAn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
